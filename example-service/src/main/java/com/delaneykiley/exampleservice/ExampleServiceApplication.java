8d20d97d2644677c553614a7e044602f486f5ba9
Kiley Delaney <kdelaney@solstice.com>
2018-09-05 16:46:02 -0500

    Tests adding sample service module to project package

---
+++ example-service/src/main/java/com/delaneykiley/exampleservice/ExampleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.delaneykiley.exampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleServiceApplication.class, args);
+    }
+}
